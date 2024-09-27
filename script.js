b5eb21178f3d7f2d7598e493afd372dae687c2e4
Yuichi <163052529+Yuichi-Aragi@users.noreply.github.com>
2024-09-28 00:31:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = {
-    proxyUrl: 'https://la.sehere2468.workers.dev/',
+    proxyUrl: 'https://sp.yukag.workers.dev/',
     targetUrl: 'https://jnovels.com/top-light-novels-to-read/',
     batchSize: 40,
     maxRetries: 3,
